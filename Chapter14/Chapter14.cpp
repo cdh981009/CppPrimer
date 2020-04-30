96c17742d09a0c21038792bc56378d362a7694a9
Cho Daeho <cdh981009@gmail.com>
2020-04-30 20:07:13 +0900

    Make basic skeleton code for chapter14.

---
+++ Chapter14/Chapter14.cpp
@@ -0,0 +1,28 @@
+// Chapter14.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+#include "Sales_data.h"
+
+using namespace std;
+
+int main() {
+	Sales_data data1, data2;
+	// 입력을 읽어 data1과 data2에 넣는 코드
+	// data1과 data2가 ISBN이 같은지 확인하고
+	//		같으면 data1과 data2의 합을 출력하는 코드
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
