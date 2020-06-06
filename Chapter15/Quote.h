a74dbabd0f735724e7b635bdb51e11ea807808a1
Daeho Cho <cdh981009@gmail.com>
2020-06-06 15:57:15 +0900

    Create Quote.h

---
+++ Chapter15/Quote.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+class Quote {
+public:
+	Quote() = default;
+	Quote(const std::string& book, double sales_price) :
+		bookNo(book), price(sales_price) { }
+	std::string isbn() const { return bookNo; }
+	virtual double net_price(std::size_t n) const {
+		return n * price;
+	}
+private:
+	std::string bookNo;
+protected:
+	double price = 0.0;
+};
