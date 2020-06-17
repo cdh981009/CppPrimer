052c640e69fe17f197f3f66b5258eb0e33b8460f
Daeho Cho <cdh981009@gmail.com>
2020-06-17 20:19:10 +0900

    Import TextQuery and QueryResult from Chapter 12 to 15.

---
+++ Chapter15/TextQuery.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <fstream>
+#include <map>
+#include <memory>
+#include <set>
+#include <string>
+#include <vector>
+
+class QueryResult;
+
+class TextQuery {
+public:
+	using line_no = std::vector<std::string>::size_type;
+	TextQuery(std::ifstream& infile);
+	QueryResult query(const std::string&) const;
+private:
+	std::shared_ptr<std::vector<std::string>> file;
+	std::map<std::string, std::shared_ptr<std::set<line_no>>> wm;
+};
