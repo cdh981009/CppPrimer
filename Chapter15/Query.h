84c22ba52da51e48cc3ad3d361fa7a15d23077e3
Daeho Cho <cdh981009@gmail.com>
2020-06-18 20:14:41 +0900

    Complete Query.h for Chapter15.

--- Chapter15/Query.h
+++ Chapter15/Query.h
@@ -23,7 +23,7 @@ class Query {
 	friend Query operator&(const Query&, const Query&);
 public:
 	Query(const std::string&);
-	QueryResult eval(const TextQuery& t) {
+	QueryResult eval(const TextQuery& t) const {
 		return q->eval(t);
 	}
 	std::string rep() const {
@@ -49,3 +49,45 @@ class WordQuery : public Query_base {
 };
 
 inline Query::Query(const std::string& s) : q(new WordQuery(s)) { }
+
+class NotQuery : public Query_base {
+	friend Query operator~(const Query&);
+	NotQuery(const Query &q) : query(q) { }
+	std::string rep() const { return "~(" + query.rep() + ")"; }
+	QueryResult eval(const TextQuery&) const;
+	Query query;
+};
+
+inline Query operator~(const Query& operand) {
+	return std::shared_ptr<Query_base>(new NotQuery(operand));
+}
+
+class BinaryQuery : public Query_base {
+protected:
+	BinaryQuery(const Query& l, const Query& r, std::string s) :
+		lhs(l), rhs(r), opSym(s) { }
+	std::string rep() const { return "(" + lhs.rep() + " " + opSym + " " + rhs.rep() + ")"; }
+	Query lhs, rhs;
+	std::string opSym;
+};
+
+class AndQuery : public BinaryQuery {
+	friend Query operator&(const Query&, const Query&);
+	AndQuery(const Query& left, const Query& right) : BinaryQuery(left, right, "&") { }
+	QueryResult eval(const TextQuery&) const;
+};
+
+inline Query operator&(const Query& lhs, const Query& rhs) {
+	return std::shared_ptr<Query_base>(new AndQuery(lhs, rhs));
+}
+
+class OrQuery : public BinaryQuery {
+	friend Query operator|(const Query&, const Query&);
+	OrQuery(const Query& left, const Query& right) : BinaryQuery(left, right, "|") { }
+	QueryResult eval(const TextQuery&) const;
+};
+
+inline Query operator|(const Query& lhs, const Query& rhs) {
+	return std::shared_ptr<Query_base>(new OrQuery(lhs, rhs));
+}
+
