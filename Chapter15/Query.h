0bc59262921f0b33c3a117cba0b78bc27c78f294
Daeho Cho <cdh981009@gmail.com>
2020-06-18 19:32:30 +0900

    Merge query-family headers into one Query.h

--- Chapter15/Query.h
+++ Chapter15/Query.h
@@ -6,8 +6,16 @@
 
 #include "TextQuery.h"
 #include "QueryResult.h"
-#include "Query_base.h"
-#include "WordQuery.h"
+
+class Query_base {
+	friend class Query;
+protected:
+	using line_no = TextQuery::line_no;
+	virtual ~Query_base() = default;
+private:
+	virtual QueryResult eval(const TextQuery&) const = 0;
+	virtual std::string rep() const = 0;
+};
 
 class Query {
 	friend Query operator~(const Query&);
@@ -29,5 +37,15 @@ private:
 std::ostream& operator<<(std::ostream& os, const Query& query) {
 	return os << query.rep();
 }
+
+class WordQuery : public Query_base {
+	friend class Query;
+	WordQuery(const std::string& s) : query_word(s) { }
+	QueryResult eval(const TextQuery& t) const {
+		return t.query(query_word);
+	}
+	std::string rep() const { return query_word; }
+	std::string query_word;
+};
 
 inline Query::Query(const std::string& s) : q(new WordQuery(s)) { }
